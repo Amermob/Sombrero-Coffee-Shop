302ec34f8f8a725174523028dffcfff135d0ff7b
Amermob <amermob5@gmail.com>
2022-10-02 19:54:03 +0300

    upload the design

---
+++ css/JS/main.js
@@ -0,0 +1,13 @@
+const translation = {
+    en: {
+        home: "home",
+        who we are?: "who we are?",
+        menu:"menu",
+    },
+
+    ar: {
+        home:"الصفحة الرئسية",
+        who we are?:"من نحن",
+        menu:"قائمة الطعام",
+    }
+}
