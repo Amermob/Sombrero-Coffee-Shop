86c2bf0c85ce824c449e07839e2b3744995cf1b6
Amer Mohamed <96571594+Amermob@users.noreply.github.com>
2023-01-25 15:06:17 +0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -9,27 +9,27 @@ window.addEventListener("load", function () {
 
 //  ****************************************************************translations //
 
-// import translations from "./translations.js";
+import translations from "./translations.js";
 
-// const languageSelector = document.querySelector("select");
-// languageSelector.addEventListener("change", (event) => {
-//   setLanguage(event.target.value);
-//   localStorage.setItem("lang", event.target.value);
-// });
+const languageSelector = document.querySelector("select");
+languageSelector.addEventListener("change", (event) => {
+  setLanguage(event.target.value);
+  localStorage.setItem("lang", event.target.value);
+});
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   const language = localStorage.getItem("lang") || "en"; // اذا لم تكن اللغة متوفرة استخدم الانجليزية
-//   setLanguage(language);
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  const language = localStorage.getItem("lang") || "en"; // اذا لم تكن اللغة متوفرة استخدم الانجليزية
+  setLanguage(language);
+});
 
-// const setLanguage = (language) => {
-//   const elements = document.querySelectorAll("[data-i18n]");
-//   elements.forEach((element) => {
-//     const translationKey = element.getAttribute("data-i18n");
-//     element.textContent = translations[language][translationKey];
-//   });
-//   document.dir = language === "ar" ? "rtl" : "ltr";
-// };
+const setLanguage = (language) => {
+  const elements = document.querySelectorAll("[data-i18n]");
+  elements.forEach((element) => {
+    const translationKey = element.getAttribute("data-i18n");
+    element.textContent = translations[language][translationKey];
+  });
+  document.dir = language === "ar" ? "rtl" : "ltr";
+};
 
 //  ****************************************************************translations //
 
@@ -46,9 +46,7 @@ btn.onclick = () => {
   }
 };
 
-//   **************************************************************for ice cream machine //
-
-//   **************************************************************// for coffee machine //
+//   ************************************************************** for coffee machine //
 
 let btnc = document.getElementById("coffee-btn");
 let coffee = document.getElementById("coffee");
@@ -60,23 +58,6 @@ btnc.onclick = () => {
     coffee.style.display = "none";
   }
 };
-
-//   **************************************************************// for coffee machine //
-
-// for both machine
-
-// let bothbtn = document.getElementById("both-btn")
-// let bothform = document.getElementById("form-w")
-
-// bothbtn.addEventListener("click", () => {
-//   if(form.style.display === "none"){
-//     form.style.display = "block";
-//   }
-//   else {
-//     form.style.display = "none";
-//   }
-// }
-// )
 
 let btnScroll = document.getElementById("btn");
 
