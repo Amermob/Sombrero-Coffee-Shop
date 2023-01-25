4b8a5459f0106a4df613ad100e09d1eb6cbf4732
Amer Mohamed <96571594+Amermob@users.noreply.github.com>
2023-01-25 15:05:56 +0300

    Update translations.js

--- JS/translations.js
+++ JS/translations.js
@@ -2,7 +2,7 @@ const translations = {
   en: {
     /////////////////////////////////////////////////nav
     home: "Home",
-    whoweare: "About",
+    whoare: "who we are?",
     menu: "menu",
     events: "events",
     delivery: "delivery options",
@@ -14,7 +14,10 @@ const translations = {
     ///////////////////////////////////////////////////nav
 
     /////////////////////////////////////////////////// landing
+    SombreroLanding: "Sombrero",
+    brighten: "will brighten your day",
     sombrero: "Sombrero",
+    whoWeAre: "who we are?",
     sombrero_means: "sombrero is a latin word which means",
     sombrero_suadi:
       "sombrero is a specialty saudi owned coffee shop which porvides only the best top quality of pure colombian coffee",
@@ -24,27 +27,60 @@ const translations = {
     Events: "Events",
     business: "for business and inquiries:",
     Preferred: "Please Choose Your Preferred Option:",
-    ice: "Select An Ice Cream Machine",
-    coffee: "Select A Coffee Machine",
-    request: "request an ice cream machine",
+    ice: "Select An Ice Cream Machine:",
+    coffeeMachine: "Select A Coffee Machine:",
+    requesT: "request an ice cream machine:",
+    requestCoffee: "request a coffee machine:",
     name: "name:",
+    email: "email",
+    phoneNumber: "phone number:",
+    representative: "representative:",
+    locationRequest: "location:",
+    eventTime: "event perioud from:",
+    to: "to:",
+
+    howManyEmployees: "how many employees:",
+    eventPerioudFrom: "event perioud from:",
+    to: "to",
+    forMoreInfo: "For More Information Please Let Us Know:",
+    collaboration: "in collaboration with:",
 
     ///////////////////////////////////////////////////// events
 
     //////////////////////////////////////////////////// delivery
 
+    deliveryOption: "delivery option",
+    sombreroPRovide: "sombrero porvides Different delivery options which are:",
+
     //////////////////////////////////////////////////// delivery
 
     ///////////////////////////////////////////////////// gallery
 
+    SombreroGallery: "Sombrero gallery",
+
     ///////////////////////////////////////////////////// gallery
 
+    ///////////////////////////////////////////////////////servies
+    ourServies: "our servies",
+
+    ///////////////////////////////////////////////////////servies
+
     ///////////////////////////////////////////////////// find us
+    findUs: "find us at",
+
+    location:
+      "location: Address: 4086 Prince Muhammad Ibn Abd Al Aziz, As Sulimaniyah, Riyadh 12242",
+    Phone: "Phone: 050 295 0376",
+    ourSocials: "ourSocials:",
 
     ///////////////////////////////////////////////////// find us
 
     //////////////////////////////////////////////////////// footer
 
+    workingHours: "working hours",
+    StT: "saturday to thursday",
+    friday: "friday",
+    reserved: "all rights reserved to sombrero 2020",
     //////////////////////////////////////////////////////// footer
   },
   ar: {
